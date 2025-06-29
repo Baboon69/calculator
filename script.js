30b21fd9959bea2b746fb575b865d57798a70784
Baboon69 <40399361+Baboon69@users.noreply.github.com>
2025-06-29 21:07:35 +0300

    add float numbers, backspace, clear. support for longer numbers

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const operationElement = document.getElementById("operation");
 const display = document.getElementById("display");
 let operationSign="", storedVariableStr="";
 let operation=null, storedVariable=null, variable="0";
-
+let actualNumber,actualStoredNumber;
+let longFlag=false,resetFlag=false,operationUpdateFlag=false;
 function UpdateDisplay(){
     if (!operation)
         operationElement.innerText="";
@@ -36,7 +37,7 @@ function divide(a, b){
 }
 
 function operate(operator, a, b){
-    if (operator===divide)
+    if ((operator===divide)&&(b===0))
         return "ERR";
     return operator(a, b)
 }
@@ -56,26 +57,39 @@ function addDigit(d){
     return;
 }
 
+function setOperation(o){
+    if (o==="+"){
+        operation=add;
+    }
+    if (o==="-"){
+        operation=subtract;
+    }
+    if (o==="*"){
+        operation=multiply;
+    }
+    if (o==="/"){
+        operation=divide;
+    }
+}
+
 function addOperation(operator){
+    if(operationUpdateFlag){
+        operationSign=operator;
+        setOperation(operator);
+        return;
+    }
+
     if (operation!==null)
         sendOperate();
-    
+    if (longFlag){
+        actualStoredNumber=actualNumber;
+        actualNumber=0;
+    }
     storedVariable=variable;
     variable="0";
     operationSign=operator;
 
-    if (operator==="+"){
-        operation=add;
-    }
-    if (operator==="-"){
-        operation=subtract;
-    }
-    if (operator==="*"){
-        operation=multiply;
-    }
-    if (operator==="/"){
-        operation=divide;
-    }
+    setOperation(operator);
     return;
 }
 
@@ -83,14 +97,54 @@ function clear(){
     operation=null;
     operationSign="";
     storedVariable="";
+    actualNumber=0;
+    actualStoredNumber=0;
     variable="0";
+    longFlag=false;
+    resetFlag=false;
+    operationUpdateFlag=false;
     return;
 }
 
 function sendOperate(){
-    const res=operate(operation,Number(storedVariable),Number(variable));
+    if(operation===null){
+        return;
+    }
+    let res;
+    if (longFlag)
+        res=operate(operation,actualStoredNumber,Number(variable));
+    
+    else
+        res=operate(operation,Number(storedVariable),Number(variable));
     clear();
+    let resStr=String(res);
+    if (resStr.length>9){
+        actualNumber=res;
+        variable=res.toExponential(5);
+        longFlag=true;
+        return;
+    }
     variable=String(res);
+}
+
+function backspace(){
+    if (variable==="0")
+        return;
+    if (variable.length===1){
+        variable="0";
+        return;
+    }
+    variable=variable.slice(0,-1);
+    return;
+}
+
+function addPoint(){
+    if(variable.includes("."))
+        return;
+
+    operationUpdateFlag=false;
+    variable+=".";
+    return;
 }
 
 buttons.addEventListener("click", (event)=>{
@@ -102,18 +156,50 @@ buttons.addEventListener("click", (event)=>{
     const digitString="1234567890";
     const operationsString="+-*/";
 
+    if (resetFlag){
+        clear();
+    }
+
+    if (target.classList.contains("amen")){
+        if (text==="A/C"){
+            clear();
+            UpdateDisplay();
+            return;
+        }
+
+        if (variable==="ERR")
+            return;
+
+        if (text==="⌫"){
+            backspace();
+            UpdateDisplay();
+            return;
+        }
+        
+        if (text==="."){
+            addPoint();
+            UpdateDisplay();
+        }
+    }
+
+    if (variable==="ERR")
+        return;
+
     if (digitString.includes(text)){
         addDigit(text);
+        operationUpdateFlag=false;
         UpdateDisplay();
     }
 
     if (operationsString.includes(text)){
         addOperation(text);
+        operationUpdateFlag=true;
         UpdateDisplay();
     }
     
     if (text==="="){
         sendOperate();
         UpdateDisplay();
+        resetFlag=true;
     }
 });
